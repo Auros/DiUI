09053fa51beda485aa1f7ed1c0d62d2b71081337
Auros Nexus <aurosnex@gmail.com>
2020-10-14 22:06:33 -0400

    Basic Config (including Zenject setup)

---
+++ DiUI/Installers/DiUIInstaller.cs
@@ -0,0 +1,16 @@
+﻿using Zenject;
+
+namespace DiUI.Installers
+{
+    public class DiUIInstaller : Installer<Config, DiUIInstaller>
+    {
+        private readonly Config _config;
+
+        public DiUIInstaller(Config config) => _config = config;
+
+        public override void InstallBindings()
+        {
+            Container.BindInstance(_config).AsSingle();
+        }
+    }
+}
--- DiUI/Plugin.cs
+++ DiUI/Plugin.cs
@@ -1,4 +1,8 @@
-﻿using IPA;
+﻿using DiUI.Installers;
+using IPA;
+using IPA.Config.Stores;
+using SiraUtil.Zenject;
+using Conf = IPA.Config.Config;
 using IPALogger = IPA.Logging.Logger;
 
 namespace DiUI
@@ -9,9 +13,11 @@ namespace DiUI
         internal static IPALogger Log { get; private set; }
 
         [Init]
-        public Plugin(IPALogger logger)
+        public Plugin(Conf conf, IPALogger logger, Zenjector zenjector)
         {
             Log = logger;
+            conf.Generated<Config>();
+            zenjector.OnMenu<DiUIInstaller>();
         }
 
         [OnEnable, OnDisable]
